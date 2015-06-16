1b15209c9d257be61225e3155f5ba0875ec6c97d
Avi Romanoff <avi@romanoff.me>
2015-06-15 22:11:18 -0400

    add expii things to fish config

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -1,10 +1,14 @@
 set -x GOPATH ~/Developer/go
-# set -x PYTHONPATH "/usr/local/lib/python2.7/site-packages/"
+set -x EXPII_ROOT /Users/aroman/Developer/expii
 set PATH "/usr/local/bin" $PATH
 set PATH "/usr/local/sbin" $PATH
 set PATH "/usr/local/share/npm/bin" $PATH
 set PATH "$GOPATH/bin" $PATH
 alias 'log'="git log --graph --abbrev-commit --decorate --date=relative --format=format:'%C(bold blue)%h%C(reset) - %C(bold green)(%ar)%C(reset) %C(white)%s%C(reset) %C(dim white)- %an%C(reset)%C(bold yellow)%d%C(reset)' --all"
+
+# Expii things
+alias prod="ssh -i ~/expii-general.pem ubuntu@10.0.25.103"
+alias stage="ssh -i ~/expii-general.pem ubuntu@10.0.31.238"
 
 # From http://stackoverflow.com/questions/7064053/add-a-relative-path-to-path-on-fish-startup
 if status --is-interactive
@@ -26,6 +30,7 @@ function v
     open -a MacVim $argv
 end
 set -x EDITOR vim
+
 
 set fish_greeting "“There are a thousand hacking at the branches of evil to one who is striking at the root.”"
 set fish_greeting "“Things don't have to change the world to be important.”"
