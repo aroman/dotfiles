d843001b8853bd48d033d787e28b417c06d4a712
Avi Romanoff <avi@romanoff.me>
2018-04-26 12:27:10 -0400

    update config.fish

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -1,10 +1,3 @@
-set -x PATH $PATH /sbin/ $HOME/Developer/anaconda3/bin $HOME/Developer/bin
-set -x LENSDIR ~/Developer/Lens
-set -x HOSTTYPE x86_64
-set -x PATH $PATH $LENSDIR/Bin/$HOSTTYPE
-set -x DYLD_LIBRARY_PATH $LENSDIR/Bin/$HOSTTYPE $DYLD_LIBRARY_PATH
-
-
 function gs
 	git status
 end
@@ -14,10 +7,9 @@ function a
 end
 
 function hack
-	atom .
+	code .
 end
 
 function gl
 	git log --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit
 end
-
