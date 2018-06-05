c8aae34c1c58ac77deb69b2350bc03644945ad66
Avi Romanoff <avi@romanoff.me>
2018-06-05 16:57:27 -0400

    fish: fix merge glitch

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -7,5 +7,3 @@ alias hack="code ."
 function gl
 	git log --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit
 end
-
-set fish_greeting ""
