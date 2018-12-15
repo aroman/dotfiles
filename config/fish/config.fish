c2cceff7f25d7a668df2c18543c22dbd5df48882
Avi Romanoff <avi@romanoff.me>
2018-12-14 21:31:59 -0800

    add fish alias for scrubbing EXIF data

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -3,6 +3,7 @@ set fish_greeting ""
 alias gs="git status"
 alias a="ag -i"
 alias hack="code ."
+alias exifscrub="exiftool -all= "
 
 function gl
 	git log --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit
