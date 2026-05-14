cee3f8a11885972858e0ab115670b28c91ae2c04
Avi Romanoff <avi@magiccircle.studio>
2026-05-14 10:18:45 -0700

    chore(noctalia): Bump to v4.7.7 to fix pipewire-related crash

--- nixos/flake.nix
+++ nixos/flake.nix
@@ -12,7 +12,7 @@
     nixos-hardware.url = "github:NixOS/nixos-hardware/master";
 
     noctalia = {
-      url = "github:noctalia-dev/noctalia-shell/v4.7.1";
+      url = "github:noctalia-dev/noctalia-shell/v4.7.7";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
